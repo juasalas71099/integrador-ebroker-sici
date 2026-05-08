2a5d889959995dae52db4dda47d15741faee0fda
SUMAINFO\juan.salas <juan.salas@sumainfo.es>
2026-05-08 12:10:03 +0200

    pruebasconexion3

--- src/main/java/com/cmscomunidades/infrastructure/adapter/cmsasistencia/ExpedienteService.java
+++ src/main/java/com/cmscomunidades/infrastructure/adapter/cmsasistencia/ExpedienteService.java
@@ -1,11 +1,16 @@
 package com.cmscomunidades.infrastructure.adapter.cmsasistencia;
 
 import com.cmscomunidades.infrastructure.adapter.cmsasistencia.dto.Expediente;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @Service
 public class ExpedienteService {
@@ -13,9 +18,7 @@ public class ExpedienteService {
     private final WebClient webClient;
 
     public ExpedienteService(@Value("${cms.login.sici.url}") String siciBaseUrl) {
-        this.webClient = WebClient.builder()
-                .baseUrl(siciBaseUrl.concat("/webService/SICI/ws"))
-                .build();
+        this.webClient = buildInsecureWebClient(siciBaseUrl);
     }
 
     public Mono<String> altaExpediente(String authorizationHeader, Expediente expediente) {
@@ -25,6 +28,24 @@ public class ExpedienteService {
                 .header(HttpHeaders.AUTHORIZATION, authorizationHeader)
                 .retrieve()
                 .bodyToMono(String.class);
+    }
+
+    private WebClient buildInsecureWebClient(String siciBaseUrl) {
+        try {
+            SslContext sslContext = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+
+            HttpClient httpClient = HttpClient.create()
+                    .secure(sslSpec -> sslSpec.sslContext(sslContext));
+
+            return WebClient.builder()
+                    .baseUrl(siciBaseUrl.concat("/webService/SICI/ws"))
+                    .clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .build();
+        } catch (Exception ex) {
+            throw new IllegalStateException("Unable to initialize insecure SICI Expediente WebClient SSL context", ex);
+        }
     }
 
 }
--- src/main/java/com/cmscomunidades/infrastructure/adapter/cmsasistencia/sendclaim/SendClaimToCMSAsistencia.java
+++ src/main/java/com/cmscomunidades/infrastructure/adapter/cmsasistencia/sendclaim/SendClaimToCMSAsistencia.java
@@ -1,11 +1,16 @@
 package com.cmscomunidades.infrastructure.adapter.cmsasistencia.sendclaim;
 
 import com.cmscomunidades.infrastructure.adapter.cmsasistencia.dto.Expediente;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @Service
 public class SendClaimToCMSAsistencia {
@@ -13,9 +18,7 @@ public class SendClaimToCMSAsistencia {
     private final WebClient webClient;
 
     public SendClaimToCMSAsistencia(@Value("${cms.login.sici.url}") String siciBaseUrl) {
-        this.webClient = WebClient.builder()
-                .baseUrl(siciBaseUrl.concat("/webService/SICI/ws"))
-                .build();
+        this.webClient = buildInsecureWebClient(siciBaseUrl);
     }
 
     public Mono<String> altaExpediente(String authorizationHeader, Expediente expediente) {
@@ -25,6 +28,24 @@ public class SendClaimToCMSAsistencia {
                 .header(HttpHeaders.AUTHORIZATION, authorizationHeader)
                 .retrieve()
                 .bodyToMono(String.class);
+    }
+
+    private WebClient buildInsecureWebClient(String siciBaseUrl) {
+        try {
+            SslContext sslContext = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+
+            HttpClient httpClient = HttpClient.create()
+                    .secure(sslSpec -> sslSpec.sslContext(sslContext));
+
+            return WebClient.builder()
+                    .baseUrl(siciBaseUrl.concat("/webService/SICI/ws"))
+                    .clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .build();
+        } catch (Exception ex) {
+            throw new IllegalStateException("Unable to initialize insecure SICI SendClaim WebClient SSL context", ex);
+        }
     }
 
 }
