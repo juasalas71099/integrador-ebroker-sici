916fa414ddaa2b417ffeb8d0caea9f0e33408013
SUMAINFO\juan.salas <juan.salas@sumainfo.es>
2026-05-08 12:04:56 +0200

    pruebas conexion

--- src/main/java/com/cmscomunidades/infrastructure/adapter/cmsasistencia/login/SICIAuthService.java
+++ src/main/java/com/cmscomunidades/infrastructure/adapter/cmsasistencia/login/SICIAuthService.java
@@ -5,29 +5,51 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 import reactor.core.publisher.Mono;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 @Service("authServiceSICI")
 public class SICIAuthService {
 
-    private final WebClient.Builder webClientBuilder;
+    private final WebClient webClient;
     private final String siciBaseUrl;
 
     public SICIAuthService(WebClient.Builder webClientBuilder,
                            @Value("${cms.login.sici.url}") String siciBaseUrl) {
-        this.webClientBuilder = webClientBuilder;
+        this.webClient = buildSiciWebClient(webClientBuilder);
         this.siciBaseUrl = siciBaseUrl;
     }
 
     public Mono<SICIAuthResponse> login(SICIAuthRequest siciAuthRequest) {
 
-        return webClientBuilder.build()
+        return webClient
                 .post()
                 .uri(siciBaseUrl.concat("/webService/SICI/ws/login"))
                 .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .bodyValue(siciAuthRequest)
                 .retrieve()
                 .bodyToMono(SICIAuthResponse.class);
+    }
+
+    private WebClient buildSiciWebClient(WebClient.Builder webClientBuilder) {
+        try {
+            SslContext sslContext = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+
+            HttpClient httpClient = HttpClient.create()
+                    .secure(sslSpec -> sslSpec.sslContext(sslContext));
+
+            return webClientBuilder
+                    .clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .build();
+        } catch (Exception ex) {
+            throw new IllegalStateException("Unable to initialize insecure SICI WebClient SSL context", ex);
+        }
     }
 
     /*
